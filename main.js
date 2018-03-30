775610549c848092b3074726be04fcae6e0c28d2
rafael <rafaelfernandez677@gmail.com>
2018-03-30 21:15:06 +0800

    First Animation

---
+++ main.js
@@ -0,0 +1,12 @@
+var red = [0,100,63];
+var orange = [40,100,60];
+var green = [75,100,40];
+var blue = [196,77,55];
+var purple = [180,50,60];
+
+var color = [red,orange,green,blue,purple];
+
+var myname = 'Call me "Pael"';
+drawName(myname, color);
+
+bounceBubbles();
